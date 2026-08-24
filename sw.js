2924c416fddd4a4a898d3fe68ffba9d473c56a00
ValeriiScz <v@basdigital.cz>
2026-08-24 19:40:50 +0200

    Add files via upload
    закрывает централ кап серию - 1

--- sw.js
+++ sw.js
@@ -18,7 +18,7 @@
    ============================================================================ */
 'use strict';
 
-const CACHE_VERSION = 'mafgamestat-v14';
+const CACHE_VERSION = 'mafgamestat-v14.1';
 const SHELL_CACHE = CACHE_VERSION + '-shell';
 const DATA_CACHE  = CACHE_VERSION + '-data';
 
